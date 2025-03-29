99e128a77f71647f663cc7b608109caf88ce72b4
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2025-03-29 17:13:48 -0300

    feat: get data from api

--- frontend/app/(tabs)/home.tsx
+++ frontend/app/(tabs)/home.tsx
@@ -1,7 +1,8 @@
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
-import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
-import Entypo from '@expo/vector-icons/Entypo';
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { useEffect, useState } from "react";
+import { TouchableOpacity } from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+import { PacienteService } from "@/services/pacientes";
 
 interface Appointment {
   id: string;
@@ -19,67 +20,91 @@ interface Question {
 }
 
 export default function Home() {
-  const [name, setName] = useState('Tuany');
-  const [todayDate, setTodayDate] = useState(new Date());
-  const [appointmentsToday, setAppointmentsToday] = useState<Appointment[]>([
-    {
-      id: '1',
-      nomePaciente: 'Ana Bolena de Almeida Gonçalves',
-      hour: '14:00',
-    },
-    {
-      id: '2',
-      nomePaciente: 'João Ricardo Gomes Albuquerque',
-      hour: '15:30',
-    },
-    {
-      id: '3',
-      nomePaciente: 'João Ricardo Gomes Albuquerque',
-      hour: '17:00',
-    },
-  ]);
-  const [question, setQuestion] = useState<Question[]>([
-    {
-      avatarUrl: 'https://avatars.githubusercontent.com/u/55458349?v=4',
-      id: '1',
-      nomePaciente: 'Miguel Oliveira',
-      hour: '14:00',
-      data: new Date(),
-      qtdQuestion: 2,
-    },
-    {
-      id: '2',
-      nomePaciente: 'João Ricardo Gomes Albuquerque',
-      hour: '15:30',
-      data: new Date(),
-      qtdQuestion: 3,
-    },
-    {
-      id: '3',
-      nomePaciente: 'Edson Arantes do Nascimento',
-      hour: '17:00',
-      data: new Date(),
-      qtdQuestion: 1,
-    },
-    {
-      id: '4',
-      nomePaciente: 'Tiago Cardoso dos Santos',
-      hour: '17:00',
-      data: new Date(),
-      qtdQuestion: 1,
-    },
-    {
-      id: '5',
-      nomePaciente: 'Suzana Herculano-Houzel',
-      hour: '17:00',
-      data: new Date(),
-      qtdQuestion: 1,
-    },
-  ]);
-  const [totalPatients, setTotalPatients] = useState(20);
-  const [patientsInEvaluation, setPatientsInEvaluation] = useState(2);
-  const [patientsInTreatment, setPatientsInTreatment] = useState(13);
-  const [patientsConcluded, setPatientsConcluded] = useState(5);
+  const [name, setName] = useState("Tuany");
+  const [nextAppointmentDate, setNextAppointmentDate] = useState<Date>();
+  const [appointmentsToday, setAppointmentsToday] = useState<Appointment[]>([]);
+  const [question, setQuestion] = useState<Question[]>([]);
+  const [totalPatients, setTotalPatients] = useState();
+  const [patientsInEvaluation, setPatientsInEvaluation] = useState();
+  const [patientsInTreatment, setPatientsInTreatment] = useState(3);
+  const [patientsConcluded, setPatientsConcluded] = useState();
+
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      try {
+        const response = await PacienteService.getDuvidas();
+        const temp: Question[] = response.map((duvida: any) => ({
+          avatarUrl: duvida.avatar_url,
+          id: duvida.cpf,
+          nomePaciente: duvida.nome,
+          hour: new Date(duvida.ultima_duvida).toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          }),
+          data: new Date(duvida.ultima_duvida),
+          qtdQuestion: duvida.qtd_duvidas_nao_lidas,
+        }));
+        setQuestion(temp);
+      } catch (error) {
+        console.error("Error fetching patients:", error);
+      }
+    };
+
+    const fetchPatients = async () => {
+      try {
+        const response = await PacienteService.getPacientes();
+        setTotalPatients(response.length);
+        setPatientsInEvaluation(
+          response.filter((patient: any) => patient.status === "emAvaliacao")
+            .length
+        );
+        setPatientsConcluded(
+          response.filter((patient: any) => patient.status === "concluido")
+            .length
+        );
+      } catch (error) {
+        console.error("Error fetching patients:", error);
+      }
+    };
+
+    const fetchConsults = async () => {
+      try {
+        const response = await PacienteService.getConsultas();
+        const temp: any = response.map((consulta: any) => ({
+          id: consulta.id,
+          nomePaciente: consulta.nome,
+          hour: new Date(consulta.data_hora).toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          }),
+          date: new Date(consulta.data_hora),
+        }));
+
+        const minDate = temp.reduce(
+          (min: any, current: any) => (current.date < min ? current.date : min),
+          temp[0].date
+        );
+
+        const filteredAppointments = temp.filter(
+          (appointment: any) =>
+            appointment.date.toDateString() === minDate.toDateString()
+        );
+
+        setAppointmentsToday(filteredAppointments);
+
+        setNextAppointmentDate(new Date(response[0].data_hora));
+        setAppointmentsToday(filteredAppointments);
+      } catch (error) {
+        console.error("Error fetching patients:", error);
+      }
+    };
+
+    fetchConsults();
+    fetchPatients();
+    fetchQuestion();
+  }, []);
 
   return (
     <ScrollView style={styles.container}>
@@ -89,21 +114,23 @@ export default function Home() {
       <View style={styles.appointmentDiv}>
         <View style={styles.appointmentDivHeader}>
           <Text style={styles.subtitle}>Agendamentos</Text>
-          <Text style={styles.secondarySubtitle}>
-            {
-              [
-                'Domingo',
-                'Segunda-feira',
-                'Terça-feira',
-                'Quarta-feira',
-                'Quinta-feira',
-                'Sexta-feira',
-                'Sábado',
-              ][todayDate.getDay()]
-            }
-            ,
-            {` ${todayDate.getDate().toString().padStart(2, '0')}/${(todayDate.getMonth() + 1).toString().padStart(2, '0')}/${todayDate.getFullYear()}`}
-          </Text>
+          {nextAppointmentDate && (
+            <Text style={styles.secondarySubtitle}>
+              {
+                [
+                  "Domingo",
+                  "Segunda-feira",
+                  "Terça-feira",
+                  "Quarta-feira",
+                  "Quinta-feira",
+                  "Sexta-feira",
+                  "Sábado",
+                ][nextAppointmentDate.getDay()]
+              }
+              ,
+              {` ${nextAppointmentDate.getDate().toString().padStart(2, "0")}/${(nextAppointmentDate.getMonth() + 1).toString().padStart(2, "0")}/${nextAppointmentDate.getFullYear()}`}
+            </Text>
+          )}
         </View>
         <View style={styles.appointmentDivBody}>
           {appointmentsToday.map((item) => (
@@ -122,15 +149,15 @@ export default function Home() {
         <View style={styles.appointmentDivFooter}>
           <TouchableOpacity
             onPress={() => {
-              console.log('oxe');
+              console.log("oxe");
             }}
             style={{
-              flexDirection: 'row',
-              alignItems: 'center',
+              flexDirection: "row",
+              alignItems: "center",
               gap: 8,
             }}
           >
-            <Text style={{ fontWeight: 'bold', fontSize: 12 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 12 }}>
               + Novo agendamento
             </Text>
           </TouchableOpacity>
@@ -140,16 +167,16 @@ export default function Home() {
       <View style={styles.patientsDiv}>
         <View
           style={{
-            flexDirection: 'row',
+            flexDirection: "row",
             gap: 8,
-            justifyContent: 'space-between',
+            justifyContent: "space-between",
           }}
         >
           <View>
             <Text
               style={{
                 fontSize: 16,
-                fontWeight: 'bold',
+                fontWeight: "bold",
               }}
             >
               Total de pacientes
@@ -157,7 +184,7 @@ export default function Home() {
             <Text
               style={{
                 fontSize: 40,
-                fontWeight: 'bold',
+                fontWeight: "bold",
               }}
             >
               {totalPatients}
@@ -171,14 +198,14 @@ export default function Home() {
           >
             <View
               style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
+                flexDirection: "row",
+                justifyContent: "space-between",
               }}
             >
               <Text>Em avaliação</Text>
               <Text
                 style={{
-                  fontWeight: 'bold',
+                  fontWeight: "bold",
                 }}
               >
                 {patientsInEvaluation}
@@ -186,14 +213,14 @@ export default function Home() {
             </View>
             <View
               style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
+                flexDirection: "row",
+                justifyContent: "space-between",
               }}
             >
               <Text>Em acompanhamento</Text>
               <Text
                 style={{
-                  fontWeight: 'bold',
+                  fontWeight: "bold",
                 }}
               >
                 {patientsInTreatment}
@@ -201,14 +228,14 @@ export default function Home() {
             </View>
             <View
               style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
+                flexDirection: "row",
+                justifyContent: "space-between",
               }}
             >
               <Text>Concluídos</Text>
               <Text
                 style={{
-                  fontWeight: 'bold',
+                  fontWeight: "bold",
                 }}
               >
                 {patientsConcluded}
@@ -219,15 +246,15 @@ export default function Home() {
         <View style={styles.patientsDivFooter}>
           <TouchableOpacity
             onPress={() => {
-              console.log('oxe');
+              console.log("oxe");
             }}
             style={{
-              flexDirection: 'row',
-              alignItems: 'center',
+              flexDirection: "row",
+              alignItems: "center",
               gap: 8,
             }}
           >
-            <Text style={{ fontWeight: 'bold', fontSize: 12 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 12 }}>
               + Cadastrar paciente
             </Text>
           </TouchableOpacity>
@@ -255,27 +282,27 @@ export default function Home() {
                     width: 35,
                     height: 35,
                     borderRadius: 25,
-                    backgroundColor: '#E7E7E7',
-                    justifyContent: 'center',
-                    alignItems: 'center',
+                    backgroundColor: "#E7E7E7",
+                    justifyContent: "center",
+                    alignItems: "center",
                   }}
                 >
                   <Text>
                     {item.nomePaciente
-                      .split(' ')
+                      .split(" ")
                       .filter(
                         (_, index, arr) =>
                           index === 0 || index === arr.length - 1
                       )
                       .map((name) => name[0])
-                      .join('')}
+                      .join("")}
                   </Text>
                 </View>
               )}
               <View
                 style={{
                   flex: 1,
-                  width: '100%',
+                  width: "100%",
                 }}
               >
                 <View style={styles.appointmentItemTextDiv}>
@@ -285,7 +312,7 @@ export default function Home() {
                     style={{
                       fontSize: 12,
                       fontWeight: 600,
-                      color: '#50525A',
+                      color: "#50525A",
                     }}
                   >
                     {item.nomePaciente}
@@ -293,20 +320,20 @@ export default function Home() {
                 </View>
                 <View
                   style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
+                    flexDirection: "row",
+                    alignItems: "center",
                     gap: 8,
                   }}
                 >
                   <Text
                     style={{
-                      color: '#50525A',
+                      color: "#50525A",
                       fontSize: 12,
                     }}
-                  >{`${item.data.getDate().toString().padStart(2, '0')}/${(item.data.getMonth() + 1).toString().padStart(2, '0')}/${item.data.getFullYear()}`}</Text>
+                  >{`${item.data.getDate().toString().padStart(2, "0")}/${(item.data.getMonth() + 1).toString().padStart(2, "0")}/${item.data.getFullYear()}`}</Text>
                   <Text
                     style={{
-                      color: '#50525A',
+                      color: "#50525A",
                       fontSize: 12,
                     }}
                   >
@@ -314,7 +341,7 @@ export default function Home() {
                   </Text>
                   <Text
                     style={{
-                      color: '#50525A',
+                      color: "#50525A",
                       fontSize: 12,
                     }}
                   >
@@ -325,8 +352,8 @@ export default function Home() {
               <View>
                 <View
                   style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
+                    flexDirection: "row",
+                    alignItems: "center",
                     gap: 8,
                   }}
                 >
@@ -335,12 +362,12 @@ export default function Home() {
                       width: 8,
                       height: 8,
                       borderRadius: 9,
-                      backgroundColor: 'black',
+                      backgroundColor: "black",
                     }}
                   />
                   <Text>
-                    {item.qtdQuestion}{' '}
-                    {item.qtdQuestion > 1 ? 'dúvidas' : 'dúvida'}
+                    {item.qtdQuestion}{" "}
+                    {item.qtdQuestion > 1 ? "dúvidas" : "dúvida"}
                   </Text>
                 </View>
               </View>
@@ -355,10 +382,10 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   header: {
-    backgroundColor: '#CCE2FF',
+    backgroundColor: "#CCE2FF",
     paddingHorizontal: 24,
     paddingTop: 47,
     paddingBottom: 14,
@@ -374,17 +401,17 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
   },
   appointmentDivHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   appointmentDivBody: {
     marginTop: 16,
   },
   appointmentDivFooter: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
+    flexDirection: "row",
+    justifyContent: "flex-end",
     marginTop: 16,
-    width: '100%',
+    width: "100%",
   },
   subtitle: {
     fontSize: 16,
@@ -393,28 +420,28 @@ const styles = StyleSheet.create({
   secondarySubtitle: {
     fontSize: 16,
     fontWeight: 800,
-    color: '#9A9A9A',
+    color: "#9A9A9A",
   },
   appointmentItemDiv: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingVertical: 12,
     paddingHorizontal: 16,
-    width: '100%',
-    backgroundColor: '#F8F9FE',
+    width: "100%",
+    backgroundColor: "#F8F9FE",
     borderRadius: 12,
     marginBottom: 8,
   },
   appointmentItemTextDiv: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   divider: {
     marginTop: 16,
     height: 1,
-    backgroundColor: '#E7E7E7',
+    backgroundColor: "#E7E7E7",
     marginHorizontal: 24,
   },
   patientsDiv: {
@@ -423,30 +450,30 @@ const styles = StyleSheet.create({
   },
   patientsDivFooter: {
     marginTop: 16,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    width: "100%",
   },
   questionDiv: {
     marginTop: 34,
     paddingHorizontal: 24,
   },
   questionDivHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   questionDivBody: {
     marginTop: 16,
   },
   questionItemDiv: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingVertical: 12,
     paddingHorizontal: 16,
-    width: '100%',
-    backgroundColor: '#F8F9FE',
+    width: "100%",
+    backgroundColor: "#F8F9FE",
     borderRadius: 12,
     gap: 8,
     marginBottom: 8,
