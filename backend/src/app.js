ef41c7fb296032df037f6048f2d2528988ca8b5d
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2025-03-29 17:12:23 -0300

    refactor: makes router be made properly

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
-var cors = require('cors')
+var cors = require("cors");
 const app = express();
-app.use(express.json()); 
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //Ellian/Progresso
-const router = require("./views/progresso.routes");
-app.use("/", router);
+const router = require("./routes/progresso.routes");
+const pacienteRoutes = require("./routes/paciente.routes");
+app.use("/progresso", router);
+app.use("/pacientes", pacienteRoutes);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
