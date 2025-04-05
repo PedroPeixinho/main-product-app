8448e5d5a37d7ca96babb670bdb7936426ba853c
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2025-04-05 16:27:51 -0300

    some changes

--- backend/src/routes/video.routes.js
+++ backend/src/routes/video.routes.js
@@ -7,19 +7,25 @@ path = require("path");
 
 // Configuração do Multer para armazenar vídeos na pasta "uploads/videos"
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../uploads/videos"));
-    },
-    filename: (req, file, cb) => {
-        const ext = path.extname(file.originalname);
-        const filename = `${Date.now()}-${Math.random().toString(36).substring(7)}${ext}`;
-        cb(null, filename);
-    }
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads/videos"));
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const filename = `${Date.now()}-${Math.random()
+      .toString(36)
+      .substring(7)}${ext}`;
+    cb(null, filename);
+  },
 });
 
 const upload = multer({ storage });
 
 // Rotas
-router.post("/patient/:cpf/exercicies/:id_exercicio/video", upload.single("video"), videoController.uploadVideo);
+router.post(
+  "/patient/:cpf/exercicies/:id_exercicio/video",
+  upload.single("video"),
+  videoController.uploadVideo
+);
 
 module.exports = router;
